7912caebed118058ea5fef7f9bf2e9ad451c4773
ayalabe <ayalla120120@gmail.com>
2021-10-04 11:07:38 +0300

    update

--- js/first_qestion.js
+++ js/first_qestion.js
@@ -1,41 +1,4 @@
 
-
-// $(document).ready(function () {
-
-
-//     // process the form
-//     $('#user_form').submit(function (event) {
-//         if(!$("#user_form").valid()) return;
-//         var gender;
-//         $('#my_image').click(function() {
-//             alert( "blabla" );
-//        });
-        
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "http://localhost:3001/Artist",
-    //         contentType: "application/json",
-    //         data: JSON.stringify({
-    //             name: $("#artist_name").val(),
-    //             id: $("#artist_id").val(),
-    //             birth_year: $("#birth_year").val(),
-    //             password: $("#password").val(),
-    //             rpassword: $("#rpassword").val(),
-    //             email: $("#email").val(),
-    //             questions: [],
-    //         }),
-    //         processData: false,
-    //         encode: true,
-    //         success: function(data, textStatus, jQxhr) {
-    //             window.location.href="home";
-    //         },
-    //         error: function(jqXhr, textStatus, errorThrown) {
-    //             alert("failed to add user");
-    //         },
-    //     });
-    //     event.preventDefault();
-    // });
-// }
   
     function addQuestion() {
         console.log("id");
