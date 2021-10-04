8ecce4b3772ee56191bb7a66baaefab031c33a7c
ayalabe <ayalla120120@gmail.com>
2021-10-04 12:10:22 +0300

    update1

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,13 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://Kvuda:Kvuda1234.@cluster0.wco49.mongodb.net/test', {
+mongoose.connect('mongodb://127.0.0.1:27017/test', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log("mongo conected!")
- 
-});
+})
