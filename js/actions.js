e17af322f1a9afedcc121a257e4d29ada3120d13
ayalabe <ayalla120120@gmail.com>
2021-10-04 12:17:18 +0300

    Revert "update1"
    This reverts commit 8ecce4b3772ee56191bb7a66baaefab031c33a7c.

--- js/actions.js
+++ js/actions.js
@@ -18,7 +18,6 @@ function addUser() { //Add new users
         },
         messages: {},
     });
-
     $("#Add_User_Form").submit(function(event) {
         if (!$("#Add_User_Form").valid()) return;
         $.ajax({
@@ -46,50 +45,6 @@ function addUser() { //Add new users
         });
         event.preventDefault();
     });
-}
-
-function listTake(){
-    // // create a JSON object
-    // const user = {
-    //     "id": 1,
-    //     "name": "John Doe",
-    //     "age": 22
-    // };
-    // // convert JSON object to string
-    // const data = JSON.stringify(user);
-
-    
-
-    // // write JSON string to a file
-    // fs.writeFile('user.json', data, (err) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     console.log("JSON data is saved.");
-    // });
-        // process the form
-        $.ajax({
-            type: 'POST', // define the type of HTTP verb we want to use (POST for our form)
-            url: 'http://localhost:3001/list', // the url where we want to POST
-            contentType: 'application/json',
-            data: JSON.stringify({
-                
-                "songs":["רישיון נהיגה","דרכון"]
-                
-            }),
-            processData: false,            
-           // dataType: 'json', // what type of data do we expect back from the server
-            encode: true,
-            success: function( data, textStatus, jQxhr ){
-                console.log("data");
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        })
-          
-        // stop the form from submitting the normal way and refreshing the page
-        event.preventDefault();
 }
 
 function addTrip(){
@@ -122,7 +77,6 @@ function addTrip(){
 }
 
 function addQuestion() {
-    let list = []
     Id = localStorage.getItem('idUser')
     console.log("id:"+ Id);
     $("#user_form").submit(function(event) {
@@ -133,7 +87,6 @@ function addQuestion() {
             contentType: "application/json",
             data: JSON.stringify({
                 genus: $("#genus").val(),
-                navigation: $("#navigation").val(),
                 cosher: $("#cosher").val(),
                 disability: $("#disability").val(),
                 hearing: $("#hearing").val(),
@@ -145,9 +98,7 @@ function addQuestion() {
             processData: false,
             encode: true,
             success: function(data, textStatus, jQxhr) {
-                if(genus)
-                    list.push('Willy');
-                window.location.href="home"; //Go to the page question
+                location.reload();
             },
             error: function(jqXhr, textStatus, errorThrown) {
                 alert("failed to add song");
@@ -158,49 +109,6 @@ function addQuestion() {
     });
         
 }
-function AddTable() {
-    Id = localStorage.getItem('idUser')
-$.ajax({
-    type: 'get', // define the type of HTTP verb we want to use (POST for our form)
-    url: 'http://localhost:3001/User/' + Id, // the url where we want to POST
-    contentType: 'application/json',
-    success: function (data) {
-       $.each(data, function( index, value ) {
-            var column = $("<tr><th>" + value +
-            '<button type="button" id="Id" href=Id class="btn btn-outline-primary" onClick="Update(\'' + Id+ '\')" /"> Update</button>'+"</td></tr>");
-            // '<button type="button" class="btn btn-outline-primary" onClick="Update(${Id},${index})"> Update</button>'+"</td></tr>");
-            $("#productTable").append(column);
-            
-         });
-         
-    },
-    error: function (err) {
-      console.log("err", err);
-    }
-
-    
-});
-}
-
-function Update(id, index) {
-    console.log('id:'+ id)
-    console.log('index'+ index)
-
-    $.ajax({
-        type: "POST",
-        url: "http://localhost:3001/Update" +id + index,
-        
-        success: function(data, textStatus, jQxhr) {
-            location.reload();
-        },
-        error: function(jqXhr, textStatus, errorThrown) { //When the data is incorrect
-            alert("failed to add user");
-        },
-    });
-    event.preventDefault();
-}
-
-
 
 function ViewSong(id) {
     $.ajax({
@@ -301,8 +209,3 @@ function deleteUserSong(id, name_song) {
 }
 
 
-onLoad = function() {
-    AddTable();
-}
-
-$(document).ready(onLoad);
