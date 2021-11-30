d46a00b42fbfa62a3d6157529c254de0b1bc2781
shirb1999 <shir220299@gmail.com>
2021-11-30 15:22:29 +0200

    make it better

--- js/forget.js
+++ js/forget.js
@@ -24,8 +24,8 @@ $(document).ready(function () {
         // process the form
         $.ajax({
             type: "GET", // define the type of HTTP verb we want to use ("GET" for our form)
-            url: 'http://localhost:3001/users/' + $("#id_field").val() + $("#username").val(), // the url where we want to POST
-            // url: 'https://kvuda.herokuapp.com/users/' + $("#id_field").val() + $("#username").val(), 
+            // url: 'http://localhost:3001/users/' + $("#id_field").val() + $("#username").val(), // the url where we want to POST
+            url: 'https://kvuda.herokuapp.com/users/' + $("#id_field").val() + $("#username").val(), 
             success: function( data, textStatus, jQxhr ){
                 alert(data);
                 window.location.href = "kvuda";
